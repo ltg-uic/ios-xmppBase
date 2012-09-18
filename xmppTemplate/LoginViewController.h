edfb9ca8f8cd4e0ec4c3cd9836271be113359a9c
Anthony Perritano <aperritano@gmail.com>
2012-09-17 22:39:16 -0500

    first commit

---
+++ xmppTemplate/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ios-xmppBase
+//
+//  Created by Anthony Perritano on 9/17/12.
+//  Copyright (c) 2012 Learning Technologies Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UITableViewController
+
+@end
