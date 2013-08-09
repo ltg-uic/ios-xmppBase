d271f939d1a884fbf00886f1b1462c79059cfc26
Anthony Perritano <aperri3@uic.edu>
2013-08-09 16:18:59 -0500

    added navigation controller

--- xmppTemplate/AppDelegate.m
+++ xmppTemplate/AppDelegate.m
@@ -36,7 +36,7 @@ BOOL isMUC = YES;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    
+        
     //only have this we are hardcoding the username
     
     //[[NSUserDefaults standardUserDefaults] setObject:@"tester@ltg.evl.uic.edu" forKey:kXMPPmyJID];
